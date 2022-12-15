102e5e9fff1a110e3ae748071221ea8723e3cc7d
Romain <romain.reghem@etud.univ-jfc.fr>
2022-12-15 18:45:49 +0100

    i

--- src/Questionnaire.jsx
+++ src/Questionnaire.jsx
@@ -9,38 +9,20 @@ const Questionnaire = () => {
     return (
         <Center gap={2} flexDirection={'column'} p={4} minH={'100%'} bgImage={'url(https://www.wallpaperup.com/uploads/wallpapers/2013/02/14/40146/a0594c187bca3b801c2f108e2741967a.jpg)'}>
             <Stack gap={4} align={'start'} boxShadow={'rgba(0, 0, 0, 0.16) 0px 3px 6px, rgba(0, 0, 0, 0.23) 0px 3px 6px;'} p={'8'} bg={'#F7FFE0'} >
+                
                 <IconButton onClick={() => navigate('/')} colorScheme={'bee'} icon={<FontAwesomeIcon icon={faHome} />}/>
-                <Heading fontWeight={'black'}>Questionnaire d'utilisabilité</Heading>
+                
+                <Heading fontWeight={'black'}>Proposer des données</Heading>
                 <Stack>
-                    <Text>Avez-vous trouvé le site facile d'utilisation ?</Text>
-                    <RadioGroup>
-                        <Stack spacing={5} direction='row'>
-                            <Radio colorScheme='green' value='1'>Oui</Radio>
-                            <Radio colorScheme='yellow' value='2'>Non</Radio>
-                        </Stack>
-                    </RadioGroup>
-                </Stack>
-                <Stack>
-                    <Text>Avez-vous trouvé ce que vous cherchiez sur le site ?</Text>
-                    <RadioGroup>
-                        <Stack spacing={5} direction='row'>
-                            <Radio colorScheme='green' value='1'>Oui</Radio>
-                            <Radio colorScheme='yellow' value='2'>Non</Radio>
-                        </Stack>
-                    </RadioGroup>
-                </Stack>
-                <Stack>
-                    <Text>Pourriez-vous dessiner un rayon du rayon 4000m sur la carte ?</Text>
-                    <RadioGroup>
-                        <Stack spacing={5} direction='row'>
-                            <Radio colorScheme='green' value='1'>Oui</Radio>
-                            <Radio colorScheme='yellow' value='2'>Non</Radio>
-                        </Stack>
-                    </RadioGroup>
-                </Stack>
-                <Stack>
-                    <Text>Si vous avez des suggestions, écrivez les ici :</Text>
+                    <Text>Donnez un titre</Text>
                     <Input placeholder="Aa" />
+                </Stack>
+                <Stack>
+                    <Text>Décrivez nous brièvement ce que représentent ces données</Text>
+                    <Input placeholder="Aa" />
+                </Stack>
+                <Stack w={'100%'} align='center' justify={'center'} py='10' border={'1px black dashed'}>
+                    <Text>Glissez vos données ici !</Text>
                 </Stack>
                 <Button onClick={() => navigate('/')} colorScheme={'green'}>Envoyer</Button>
             </Stack>
--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-const BASE_URL = 'http://127.0.0.1:3000/api';
-// const BASE_URL = 'https://beeo.herokuapp.com/api';
+// const BASE_URL = 'http://127.0.0.1:3000/api';
+const BASE_URL = 'https://beeo.herokuapp.com/api';
 
 export default axios.create({
     baseURL : BASE_URL
